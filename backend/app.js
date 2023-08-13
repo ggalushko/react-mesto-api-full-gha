ea96ca8475f7414d5387c75aa4d266c4adb495a9
ggalushko <titanmelchior@gmail.com>
2023-08-13 16:58:46 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrate, Joi } = require('celebrate');
@@ -10,11 +11,11 @@ const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { auth } = require('./middlewares/auth');
-const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+app.use(cors({ origin: ['http://127.0.0.1:3000', 'https://mestofull.nomoreparties.co', 'https://api.mestofull.nomoreparties.co'], credentials: true, maxAge: 30 }));
 
 app.use(express.json());
 
