a8555e739f5f6993871d21b32f6b94e38188812e
ggalushko <titanmelchior@gmail.com>
2023-08-13 20:22:29 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
@@ -22,7 +23,6 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.use(cors);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
